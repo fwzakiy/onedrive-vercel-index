3c4a96c5251f09008b4d7c5eb9e87666fc1bb56f
Faishal Wafiq Zakiy <fwzakiy@gmail.com>
2023-03-05 22:54:49 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -60,5 +60,5 @@ module.exports = {
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
   // without timezone and replacing T with space.
-  datetimeFormat: 'l, j F Y - H:i \W\I\B',
+  datetimeFormat: 'DD/MM/YYYY, HH:mm:ss,
 }
